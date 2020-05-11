9f0203a761671ee7a9a187ce7b374c2bf2c1c985
wuzm <l>
2020-05-12 00:55:46 +0800

    添加openfeign组件

--- src/main/java/com/daniel/webflux/example/WebFluxTestApplication.java
+++ src/main/java/com/daniel/webflux/example/WebFluxTestApplication.java
@@ -2,6 +2,7 @@ package com.daniel.webflux.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date: 2020/3/31 13:04
  */
 @SpringBootApplication
+@EnableFeignClients
 public class WebFluxTestApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/daniel/webflux/example/client/FeignTestClient.java
@@ -0,0 +1,11 @@
+package com.daniel.webflux.example.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * @author daniel
+ * @date 2020/5/12 0:40
+ */
+@FeignClient
+public class FeignTestClient {
+}
